2a08806b9827349df5e66ecd50ebb0f2e6fa4fd2
hoethan231 <Icydragons1357@gmail.com>
2024-02-04 21:12:17 -0800

    update api endpoints

--- src/sqliteDB.py
+++ src/sqliteDB.py
@@ -1,28 +1,37 @@
 import sqlite3
 
-connection = sqlite3.connect("../database.db")
+def connect():
+    return sqlite3.connect("../database.db")
 
 def create_table():
+    connection = connect()
     with connection:
-        connection.execute("""CREATE TABLE urls (
+        connection.execute("""CREATE TABLE IF NOT EXISTS urls (
             _id INTEGER PRIMARY KEY,
             url TEXT,
             alias TEXT,
             timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);""")
         
+        
 def add_url(url, alias):
+    connection = connect()
     with connection:
         connection.execute("INSERT INTO urls (url, alias) VALUES (?,?);", (url, alias))
         
-def delete_url(url):
+def delete_url(alias):
+    connection = connect()
     with connection:
-        connection.execute("DELETE FROM urls WHERE url = ?;", (url, ))
+        connection.execute("DELETE FROM urls WHERE alias = ?;", (alias, ))
         
 def get_url(alias):
+    connection = connect()
     with connection:
-        return connection.execute("SELECT * FROM urls WHERE alias = ?;", (alias, )).fetchall()
+        return connection.execute("SELECT * FROM urls WHERE alias = ?;", (alias, )).fetchall()   
         
 def get_all_urls():
+    connection = connect()
     with connection:
-        return connection.execute("SELECT * FROM urls;").fetchall()
+        urls = connection.execute("SELECT * FROM urls;").fetchall()
+        output = [{"id": row[0], "url": row[1], "alias": row[2]} for row in urls]       
+        return output
     

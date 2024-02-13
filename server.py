9951bccdc895fd7f1f0f7f13fcb0b4f5b73ffa4d
hoethan231 <Icydragons1357@gmail.com>
2024-02-13 00:29:59 -0800

    add hashstring for missing alias

--- server.py
+++ server.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI, Request
 import uvicorn
 import sqliteDB as helpers
 
+
 app = FastAPI()
 
+db = "database.db"
 
 @app.get("/")
 def root():
@@ -15,18 +17,22 @@ async def create_url(request: Request):
     
     data = await request.json()
     
-    if helpers.already_exist(request["alias"]):
+    if "alias" not in data:
+        data["alias"] = helpers.generateHash()
+    
+    
+    if helpers.already_exist(db, data["alias"]):
         return { "message": "please create a unique alias"}    
     else:
-        helpers.add_url(request["url"], request["alias"])
-        return { "url": request["url"], "alias": request["alias"] }
+        helpers.add_url(db, data["url"], data["alias"])
+        return { "url": data["url"], "alias": data["alias"] }
     
 
 @app.get("/delete_url/{alias}")
 def delete_url(alias: str):
     
-    if helpers.already_exist(alias):
-        helpers.delete_url(alias)
+    if helpers.already_exist(db, alias):
+        helpers.delete_url(db, alias)
         return { "message": "the shorten url has be deleted successfully"}    
     else:
         return { "message": "the url is not found" }
@@ -35,7 +41,7 @@ def delete_url(alias: str):
 @app.get("/find/{alias}")
 def find_url(alias: str):
     try: 
-        return {"url": helpers.get_url(alias) }
+        return {"url": helpers.get_url(db, alias) }
     
     except:
         return { "message": "the url is not found" }
@@ -43,7 +49,7 @@ def find_url(alias: str):
 
 @app.get("/list_all")
 def list_all():
-    return helpers.get_all_urls()
+    return helpers.get_all_urls(db)
 
 
 if __name__ == "__main__":
--- sqliteDB.py
+++ sqliteDB.py
@@ -1,4 +1,5 @@
 import sqlite3
+import hashlib
 
 def create_table(db_file: str):
     try:
@@ -39,7 +40,7 @@ def get_url(db_file, alias):
         with connection:
             return cursor.execute("SELECT * FROM urls WHERE alias = ?;", (alias, )).fetchall() 
     except:
-        print("some err")
+        return print("some err")
         
 def get_all_urls(db_file):
     try:
@@ -50,14 +51,20 @@ def get_all_urls(db_file):
             output = [{"id": row[0], "url": row[1], "alias": row[2]} for row in urls]       
             return output 
     except:
-        print("some err")
+        return print("some err")
         
-def already_exist(db_file, url):
+def already_exist(db_file, alias):
     try:
         connection = sqlite3.connect(db_file)
         cursor = connection.cursor()
         with connection:
-            return (len(cursor.execute("SELECT * FROM urls WHERE alias = ?", (url,)).fetchall()) != 0)
+            return (len(cursor.execute("SELECT * FROM urls WHERE alias = ?", (alias,)).fetchall()) != 0)
     except:
-        print("some err")
+        return print("some err:")
         
+def generateHash(url, timestamp):
+    
+    combined_string = url + timestamp
+    return hashlib.sha256(combined_string.encode()).hexdigest()[0:5]
+
+

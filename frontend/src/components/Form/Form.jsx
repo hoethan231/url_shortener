80153300503c8f594000727d968a5850cfb1891e
hoethan231 <Icydragons1357@gmail.com>
2024-05-30 00:50:36 -0700

    update form

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -8,8 +8,11 @@ const Form = ({onSearch}) => {
         <form className="form-wrapper">
             <div className="form-header"><h2>Create Link</h2></div>
             <div className="form-body">
-                <label>Enter Long URL Here:</label>
+                <label>Alias Here:</label>
                 <Searchbar onSearch={onSearch}/>
+                <label>Long URL Here:</label>
+                <Searchbar onSearch={onSearch}/>
+                <h7 className="error-message">That alias has already been used by this particular site :(</h7>
                 <button type="submit">Create!</button>
             </div>
         </form>

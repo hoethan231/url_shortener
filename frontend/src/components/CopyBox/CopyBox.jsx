2a16e9c7dcb54f536557d911c183fd4cebb26517
hoethan231 <Icydragons1357@gmail.com>
2024-05-29 19:53:23 -0700

    implement copy box

---
+++ frontend/src/components/CopyBox/CopyBox.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./CopyBox.css";
+
+const CopyBox = ({ label, link }) => {
+    
+    const [clicked, setClicked] = useState(false);
+    
+    const handleCopy = () => {
+        setClicked(true);
+        navigator.clipboard.writeText(link);
+        setTimeout(() => {
+            setClicked(false);
+        }, 2000)
+    }
+
+    return (
+        <div className="container">
+            <div className="label">
+                {label}
+            </div>
+            <div className="copy-text">
+                <input type="text" className="text" value={link} readOnly/>
+                <button onClick={handleCopy}>
+                    <i className={clicked ? "fa-solid fa-check" : "fa-regular fa-copy"}></i>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default CopyBox;
